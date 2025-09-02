13d98eb5bccee917bf6535da7717decfbc6e60e9
Matthew R. Becker <beckermr@users.noreply.github.com>
2025-09-02 12:33:20 -0500

    fix: ensure we specify new class attributes

--- src/rattler_build_conda_compat/yaml.py
+++ src/rattler_build_conda_compat/yaml.py
@@ -27,11 +27,17 @@ def _yaml_object() -> YAML:
     yaml = YAML(typ="rt")
 
     class _CustomConstructor(yaml.Constructor)
-        pass
+        yaml_constructors = {}
+        yaml_multi_constructors = {}
+
+    class _CustomRepresenter(yaml.Representer)
+        yaml_representers = {}
+        yaml_multi_representers = {}
 
     yaml.Constructor = _CustomConstructor
     yaml.Constructor.add_constructor("tag:yaml.org,2002:float", float_as_string_constructor)
-    yaml.representer.add_representer(str, _yaml_represent_str)
+    yaml.Representer = _CustomRepresenter
+    yaml.Representer.add_representer(str, _yaml_represent_str)
     yaml.allow_duplicate_keys = False
     yaml.preserve_quotes = True
     yaml.width = 320
